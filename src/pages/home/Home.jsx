fa3a1b9bd0878b0ad94a79cced6deda5aebbb4ee
taiatulislam <taiatulislamapon@gmail.com>
2024-04-21 01:18:56 +0600

    banner section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,9 @@
+import Banner from "./banner/Banner";
 
 function Home() {
   return (
-    <div className="bg-gray-100">
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Your Company!</h1>
-        <p className="text-lg">
-          We provide high-quality services to meet all your needs.
-        </p>
-        <p className="text-lg mt-4">Contact us today to get started.</p>
-      </div>
+    <div className="max-w-7xl mx-auto">
+      <Banner />
     </div>
   );
 }
