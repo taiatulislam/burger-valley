630fa62e161609366b73cf1f7232bcdaba340be4
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-04-23 21:33:09 +0600

    added faq page

---
+++ src/pages/faq/Faq.jsx
@@ -0,0 +1,12 @@
+import Faqs from "../../Components/FaqComponents/Faqs";
+
+
+const Faq = () => {
+    return (
+        <div>
+            <Faqs />
+        </div>
+    );
+};
+
+export default Faq;
